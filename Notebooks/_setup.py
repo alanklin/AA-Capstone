bd85c3f46793f0caa9b5945ad730a5e2a3ab8bbd
txpetrin <txpetrin@mtu.edu>
2025-01-31 12:08:33 -0500

    _setup file paths

--- Notebooks/_setup.py
+++ Notebooks/_setup.py
@@ -4,7 +4,21 @@
 # variable names. Should be called at the top of the notebooks.
 
 # @author AL, TP
+
+# File paths
+from pathlib import Path
+# Get the absolute path to the current script (_setup.py)
+script_dir = Path(__file__).resolve().parent
+
+# Get the project root (assuming Notebooks is at the same level as data)
+project_root = script_dir.parent
+
+# Construct the path to data.csv
+sector_data_csv_path = project_root / "Data" / "Border_Sector_Data.csv"
+state_data_csv_path = project_root / "Data" / "Border_State_Data.csv"
+
+# Data manipulation
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-import os
+import sys, os
