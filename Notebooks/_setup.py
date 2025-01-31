58b7353c89181878aeac001ba6a98e6dfc2cf488
txpetrin <txpetrin@mtu.edu>
2025-01-31 11:45:43 -0500

    _setup.py
    Create setup file

---
+++ Notebooks/_setup.py
@@ -0,0 +1,10 @@
+
+### _setup.py ###
+# This file is used to handle the imports of the notebooks for ease of use and consistency with 
+# variable names. Should be called at the top of the notebooks.
+
+# @author AL, TP
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import os
