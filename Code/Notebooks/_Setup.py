4ed3bbac3947b7b7e52c0da0ad2a124e505f4c53
txpetrin <txpetrin@mtu.edu>
2025-02-02 18:39:43 -0500

    EDA Sector Data Push

--- Code/Notebooks/_Setup.py
+++ Code/Notebooks/_Setup.py
@@ -14,11 +14,17 @@ script_dir = Path(__file__).resolve().parent
 project_root = script_dir.parent.parent
 
 # Construct the path to data.csv
-sector_data_csv_path = project_root / "Data" / "Border_Sector_Data.csv"
+sector_data_csv_path = project_root / "Data" / "sector_encounters_fy_2020_2025.csv"
 state_data_csv_path = project_root / "Data" / "Border_State_Data.csv"
+
 
 # Data manipulation
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 import sys, os
+import seaborn as sns
+
+# For mapping use:
+import geopandas as gpd
+import geopandas.tools
