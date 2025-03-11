fe96ad2db1832d31cc14d34b9240169f11e556b7
Alan <linbbn@bc.edu>
2025-03-11 02:20:08 -0400

    Update _Functions.py

--- Code/Notebooks/_Functions.py
+++ Code/Notebooks/_Functions.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 # Create a dictionary mapping month abbreviations (uppercase) to month numbers
 month_abbr_to_num = {
     'JAN': 1, 'FEB': 2, 'MAR': 3, 'APR': 4, 'MAY': 5, 'JUN': 6,
